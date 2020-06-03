a7c6bcf71ee30a5d52a0830699be377814da02d1
Karim Drief <karim_drief1@hotmail.fr>
2020-06-03 16:33:05 +0200

    Update and edit methods

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,4 +2,42 @@ class CarsController < ApplicationController
   def index
     @cars = Car.all
   end
+
+  def show
+    @car = Car.find(params[:id])
+  end
+
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+    @car.user = current_user
+    if @car.save
+      redirect_to cars_path(@car)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @car = Car.find(params[:id])
+  end
+
+  def update
+    @car = Car.find(params[:id])
+    @car.update(car_params)
+
+    redirect_to car_path(@car)
+  end
+
+  def delete
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:model, :brand, :year, :description, :place, :price_per_day, :photo)
+  end
 end
